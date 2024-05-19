589514286a2f7cb61d5ec114ef7fc6e756b80bea
Dafraouidev <dafraouimehdi05@gmail.com>
2024-05-19 20:31:56 +0100

    commit all files

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculate from './ReduxAppNotaria/Calculate';
+import { Provider } from 'react-redux';
+import store from './ReduxAppNotaria/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <Provider store = {store}>
+      <Calculate />
+    </Provider>
+
+   
+
+    {/* <App /> */}
   </React.StrictMode>
 );
 
